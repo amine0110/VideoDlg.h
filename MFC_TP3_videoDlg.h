3e178b5b6045ff6b12a93c8de5824cd48691cb4a
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2020-01-10 23:21:28 +0100

    Update MFC_TP3_videoDlg.h

--- MFC_TP3_videoDlg.h
+++ MFC_TP3_videoDlg.h
@@ -1,11 +1,11 @@
 
-// MFC_TP3_videoDlg.h : fichier d'en-tête
+// MFC_TP3_videoDlg.h : fichier d'en-tÃªte
 //
 
 #pragma once
 #include <Vfw.h>
 
-// boîte de dialogue CMFC_TP3_videoDlg
+// boÃ®te de dialogue CMFC_TP3_videoDlg
 class CMFC_TP3_videoDlg : public CDialogEx
 {
 // Construction
@@ -16,15 +16,17 @@ public:
 	unsigned char * Imrgb = NULL;
 	
 	
-	//************* fonctions membres
 	static LPVOID CALLBACK DetectionContour(HWND hWndC, LPVIDEOHDR pV);
 	static LPVOID CALLBACK DetectionContour2(HWND hWndC, LPVIDEOHDR pV);
 	static LPVOID CALLBACK InverserImage(HWND hWndC, LPVIDEOHDR pV);
 	static LPVOID CALLBACK SeuillageImage(HWND hWndC, LPVIDEOHDR pV);
+	static LPVOID CALLBACK rotation(HWND hWndC, LPVIDEOHDR pV);
+	static LPVOID CALLBACK flou(HWND hWndC, LPVIDEOHDR pV);
+	static LPVOID CALLBACK rot(HWND hWndC, LPVIDEOHDR pV);
 
 
 
-// Données de boîte de dialogue
+// DonnÃ©es de boÃ®te de dialogue
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_MFC_TP3_VIDEO_DIALOG };
 #endif
@@ -33,11 +35,11 @@ public:
 	virtual void DoDataExchange(CDataExchange* pDX);	// Prise en charge de DDX/DDV
 
 
-// Implémentation
+// ImplÃ©mentation
 protected:
 	HICON m_hIcon;
 
-	// Fonctions générées de la table des messages
+	// Fonctions gÃ©nÃ©rÃ©es de la table des messages
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
@@ -47,7 +49,6 @@ public:
 	afx_msg void OnBnClickedOk();
 	afx_msg void OnBnClickedinit();
 	afx_msg void OnBnClickedvideoformat();
-//	afx_msg void OnBnClickedvideoformat2();
 	afx_msg void OnBnClickedvideosource();
 	afx_msg void OnBnClickedtraitement();
 	//********************
@@ -55,4 +56,6 @@ public:
 	afx_msg void OnBnClickedinversion();
 	afx_msg void OnBnClickedseuillage();
 	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedrotation();
+	afx_msg void OnBnClickedflou();
 };
